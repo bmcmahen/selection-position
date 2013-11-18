df385018015ab7f68ba12cafb90a34eff2ca26e4
bmcmahen <ben.mcmahen@gmail.com>
2013-11-17 17:00:14 -0700

    support cursor position for when there isnt a selection

--- index.js
+++ index.js
@@ -4,11 +4,25 @@
  */
 
 module.exports = function(){
+
   if (window.getSelection){
     var selection = window.getSelection();
     if (!selection.rangeCount) return;
-    return selection.getRangeAt(0).getBoundingClientRect();
-  } else if (document.selection){
+
+    var range = selection.getRangeAt(0);
+
+    if (selection.type == 'Range'){
+      return range.getBoundingClientRect();
+    }
+    
+    // if we only have a cursor, then we need to insert
+    // a dummy element and see what's what.
+    var dummy = document.createElement('span');
+    range.insertNode(dummy);
+    return dummy.getBoundingClientRect();
+  } 
+
+  if (document.selection){
     var txtRange = document.selection.createRange();
     return txtRange.getBoundingClientRect();
   } 
